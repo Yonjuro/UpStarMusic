47cedc96f6fac280d4ce7218530025e21a24dd35
John Bergdahl <john@bergdahl.co>
2018-03-07 23:46:55 +0100

    Add all queries

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -9,4 +9,44 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+    const query = Artist
+        .find(buildQuery(criteria))
+        .sort({[sortProperty]: 1})
+        .skip(offset)
+        .limit(limit);
+    
+    return Promise.all([query, Artist.find(buildQuery(criteria)).count()])
+        .then((result) => {
+            return {
+                all: result[0],
+                count: result[1],
+                offset,
+                limit
+            };
+        });
 };
+
+var buildQuery = (criteria) => {
+    var q = {};
+    
+    if(criteria.age) {
+        q.age = { 
+            $gte: criteria.age.min, 
+            $lte: criteria.age.max 
+        };
+    }
+
+    if(criteria.yearsActive) {
+        q.yearsActive = { 
+            $gte: criteria.yearsActive.min, 
+            $lte: criteria.yearsActive.max 
+        };
+    }
+
+    if(criteria.name) {
+        q.$text = { $search : criteria.name };
+    }
+
+    return q;
+};
+
